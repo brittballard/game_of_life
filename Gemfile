7ea55fe562787cba600ea18fcb0071fcc6654078
Britton Ballard <davidbritton.ballard@gmail.com>
2012-02-15 23:03:09 -0600

    Trying to get mongodb up and running on heroku.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source 'http://rubygems.org'
 gem 'sinatra'
+gem "mongo"
+gem "bson"
+gem "bson_ext"
--- game_of_life.rb
+++ game_of_life.rb
@@ -1,5 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'uri'
+require 'mongo'
+
+uri = URI.parse(ENV['MONGOHQ_URL'])
+conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+db = conn.db(uri.path.gsub(/^\//, ''))
 
 get '/' do
   "Are you ready for the worlds sexiest game of life?"
