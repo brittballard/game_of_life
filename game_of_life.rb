ee6fa8bcbf4816ffde46355630c1b17bb92c5552
Britton Ballard <davidbritton.ballard@gmail.com>
2012-02-17 20:35:17 -0600

    Added some of the js stuff to get started. Working on the first spec (for the board).

--- game_of_life.rb
+++ game_of_life.rb
@@ -3,10 +3,10 @@ require 'sinatra'
 require 'uri'
 require 'mongo'
 
-uri = URI.parse(ENV['MONGOHQ_URL'])
-conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-db = conn.db(uri.path.gsub(/^\//, ''))
-
 get '/' do
-  "Are you ready for the worlds sexiest game of life?"
+  erb :game_of_life
+end
+
+get '/specs' do
+  erb :jasmine
 end
---
+++ public/javascripts/backbone/models/board.js
@@ -0,0 +1,9 @@
+var gameOfLife.board = Backbone.Model.extend({
+  cells: {},
+  setLiveCell: function(x, y){
+    
+  },
+  setDeadCell: function(x, y){
+    
+  }
+})
