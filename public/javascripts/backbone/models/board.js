4afcd7ae99d4bebf8a9a6496a3b9408c351ccd18
Britton Ballard <davidbritton.ballard@gmail.com>
2012-02-17 21:38:36 -0600

    setLiveCell method now adds an object with the status value of alive to the dictionary with a key that coresponds to the cooridinates.

--- public/javascripts/backbone/models/board.js
+++ public/javascripts/backbone/models/board.js
@@ -1,7 +1,7 @@
 GameOfLife.Board = Backbone.Model.extend({
   cells: {},
   setLiveCell: function(x, y){
-    
+    this.cells[x.toString() + ',' + y.toString()] = { status: 'alive' }
   },
   setDeadCell: function(x, y){
     
--- public/javascripts/jasmine/specs/boardSpec.js
+++ public/javascripts/jasmine/specs/boardSpec.js
@@ -3,5 +3,25 @@ describe('board', function(){
   
   it('should contain a cells property', function(){
     expect(board.cells).toBeDefined();
-  })
-})
+  });
+  
+  it('should contain a method called \'setLiveCell\'', function(){
+    expect(board.setLiveCell).toBeDefined();
+  });
+  
+  it('should contain a method called \'setDeadCell\'', function(){
+    expect(board.setDeadCell).toBeDefined();
+  });
+  
+  describe('setLiveCell', function(){
+    it('should add a new value to the cells dictionary with a key that is the x/y cordinates of the cell', function(){
+      board.setLiveCell(1,2);
+      expect(board.cells['1,2']).toBeDefined();
+    });
+    
+    it('should set the new cell\'s status to \'alive\'', function(){
+      board.setLiveCell(1,2);
+      expect(board.cells['1,2'].status).toEqual('alive');
+    });
+  });
+});
