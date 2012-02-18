f93bdf2163189f57f4ed87e249183a2d7560c55b
Britton Ballard <davidbritton.ballard@gmail.com>
2012-02-17 22:48:33 -0600

    Built out and included cells spec.

--- public/javascripts/jasmine/specs/cellSpec.js
+++ public/javascripts/jasmine/specs/cellSpec.js
@@ -1,3 +1,19 @@
 describe('cell', function(){
+  var cell = new GameOfLife.Cell();
   
+  it('should have an x property that is undefined', function(){
+    expect(cell.x).toBeUndefined();
+  });
+  
+  it('should have a y property that is undefined', function(){
+    expect(cell.y).toBeUndefined();
+  });
+  
+  it('should have a statu property that is undefined', function(){
+    expect(cell.status).toBeUndefined();
+  });
+  
+  it('should have a nextGenerationStatus property that is undefined', function(){
+    expect(cell.nextGenerationStatus).toBeUndefined();
+  });
 });
