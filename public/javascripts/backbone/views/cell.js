839ad8477771b134ceb9831214099d9f5b805851
Britton Ballard <davidbritton.ballard@gmail.com>
2012-02-25 21:08:47 -0600

    Added the first event.

--- public/javascripts/backbone/views/cell.js
+++ public/javascripts/backbone/views/cell.js
@@ -1,9 +1,24 @@
 GameOfLife.Views.Cell = Backbone.View.extend({
+
   id: "app",
+
   className: "cell",
+
+  events: {
+      "click": "toggleStatus"
+    },
+
+  model: new GameOfLife.Models.Cell(),
+
+  template: _.template("<div id='cell-<%= this.model.cid %>'><%= this.model.alive ? 'alive' : 'dead' %></div>"),
+
   render: function(){
-    this.$el.text(this.template({ name: 'britton' }));
+    this.$el.html(this.template({ name: 'britton' }));
     return this;
   },
-  template: _.template('"big-po-ppa no info for the dea" said <%= name %>.')
+  
+  toggleStatus: function(){
+    this.model.alive = !this.model.alive;
+    this.render();
+  }
 });
